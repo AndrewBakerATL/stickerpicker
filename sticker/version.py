e06fd4afcb5fe1f24bd88ed5c423fafc5b10aa5a
andrewbakeratl <andrewbakeratl@outlook.com>
2024-06-09 01:38:59 -0400

    big update

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '225c6fe9'
-version = '0.1.0+dev.225c6fe9'
-linkified_version = '0.1.0+dev.[225c6fe9](https://github.com/maunium/stickerpicker/commit/225c6fe9373967418e896e2c673da59d5696e834)'
+git_revision = 'b725832e'
+version = '0.1.0+dev.b725832e'
+linkified_version = '0.1.0+dev.[b725832e](https://github.com/maunium/stickerpicker/commit/b725832e83caefd5f614b0a1ed1885b8c43e7918)'
